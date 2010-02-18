dbf20cd587ceaa3fe046b70d0ff504bdfd7ddc0d
Gustavo Narea <me@gustavonarea.net>
2010-02-18 16:14:40 +0000

    Implemented the admin_media recipe

--- djangout.py
+++ djangout.py
@@ -18,7 +18,39 @@
 Buildout recipes for Buildout.
 
 """
+from os import path
+from logging import getLogger
+
+from pkg_resources import working_set
+from zc.recipe.egg import Eggs
+
+_LOGGER = getLogger(__name__)
 
 
 class AdminMediaRecipe(object):
-    pass
+    """
+    Recipe to make available the path to the media files in Django Admin.
+    
+    """
+    
+    def __init__(self, buildout, name, options):
+        if "eggs" not in options:
+            # If we don't set the "eggs" option, it will take the part name:
+            options['eggs'] = ""
+            _LOGGER.debug("Set the 'eggs' option to an empty string")
+        ws = Eggs(buildout, name, options).working_set(["django >= 1.0"])[1]
+        for dist in ws:
+            working_set.add(dist)
+        
+        # Finding the path to the media in Django Admin:
+        from django import __file__ as django_file
+        django_root = path.dirname(django_file)
+        admin_media = path.join(django_root, "contrib", "admin", "media")
+        # We found it, let's make it available to other recipes/parts:
+        options['admin_media_root'] = admin_media
+    
+    def install(self):
+        return ()
+    
+    def update(self):
+        return ()
